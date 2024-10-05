7e1a2fb1d70b66accb5ccd5576f96f7702b1e82d
NotASuspiciousAccount <77948710+NotASuspiciousAccount@users.noreply.github.com>
2024-10-04 21:44:20 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,11 @@
 from collections import Counter
-from flask import Flask
+import csv
+import sqlite3
+
+from flask import Flask, request, g
+
+DATABASE = '/var/www/html/flaskapp/natlpark.db'
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,3 +22,26 @@ def count_me(input_str):
     for letter, count in input_counter.most_common():
         response.append('"{}": {}'.format(letter, count))
     return '<br>'.join(response)
+
+app.config.from_object(__name__)
+
+def connect_to_database():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def execute_query(query, args=()):
+    cur = get_db().execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
